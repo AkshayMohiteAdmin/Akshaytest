4f783c96d2e4428e7345ca76742368a25afee397
base <base@DESKTOP-K7698RM>
2023-02-23 11:36:23 +0530

    First Commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author base
+ *
+ */
+module Akshaytest {
+}
---
+++ src/qctest/helloworld.java
@@ -0,0 +1,10 @@
+package qctest;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Tesyed by qc");
+
+	}
+
+}
